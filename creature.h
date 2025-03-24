171c99366b6ee4f06ebe75b99bc52b2c5ab40ab3
Samuel Rieger <samueljrieger@gmail.com>
2025-03-24 10:38:44 -0600

    added creature class members and copy constructor

--- creature.h
+++ creature.h
@@ -2,11 +2,63 @@
 #define __CREATURE_H__
 
 
+#include <vector>
+#include "food.h"
+#include "environment.h"
+
+
 class Creature {
 
     public:
 
+    /**
+     * @brief Construct a new Creature object; parameterized constructor, only used to create a species model
+     * 
+     * @param totalDurability creature's durability
+     * @param strength creature's ability to fight
+     * @param defense creature's ability to avoid being hurt
+     * @param stomachCapacity amount of food creature can hold in stomach
+     * @param fatCapacity amount of food creature can store in reserves
+     */
+    Creature(unsigned int totalDurability, unsigned int strength, unsigned int defense, unsigned int stomachCapacity, unsigned int fatCapacity);
+
+
+    /**
+     * @brief Construct a new Creature object; copy constructor, used to instantiate members of the species
+     * 
+     * @param species copied object
+     */
+    Creature(Creature & species);
+
+    // copy operator
+    Creature & operator = (const Creature& CREATURE);
+
+    // deconstructor
+    ~Creature();
+
     private:
+
+    Creature * _species; // pointer to the creature's species model object
+
+
+    unsigned int _totalDurability; // equivalent to " max health"
+    unsigned int _durability; // equivalent to "health"
+    unsigned int _strength; // how good creature is at killing
+    unsigned int _defense; // how good creature is at surviving
+
+
+    vector<Food> _edibleFoods;
+
+    Food _stomachFood; // food currently eating
+    unsigned int _stomachCapacity; // how large the stomach is
+    unsigned int _stomachFillAmount; // how full the creature is
+    unsigned int _fat; // fat in creature
+    unsigned int _fatCapacity; // total possible fat
+
+
+
+    Environment * _location; // pointer to current location
+    
 
 
 };
